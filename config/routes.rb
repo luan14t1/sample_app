37fa9c09b23728cd325448a028bff5760f8deccb
luan14t1e <you@example.com>
2018-01-15 16:02:54 +0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
-  scope "(:locale)", locale: /en|vi/ do
-  root to: "static_pages#home"
-  end
+  root "static_pages#home"
+  get  "/help",    to: "static_pages#help"
+  get  "/about",   to: "static_pages#about"
+  get  "/contact", to: "static_pages#contact"
+  get  "/sign_up", to: "users#new"
 end
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get sign_up_path
+    assert_response :success
+  end
+end
