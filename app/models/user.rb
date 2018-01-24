a73fa081405aedf739c76d446f3f6766721b1593
luan14t1e <you@example.com>
2018-01-24 14:55:27 +0700

    Advanced Login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save{self.email = email.downcase}
   validates :name, presence: true, length:
     {maximum: Settings.user.name.max_length}
@@ -12,4 +13,38 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length:
     {minimum: Settings.user.password.min_length}
+
+  class << self
+    # Returns the hash digest of the given string.
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    # Returns a random token.
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attributes(remember_digest: User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attributes(remember_digest: nil)
+  end
 end
